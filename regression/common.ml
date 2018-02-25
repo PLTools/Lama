3c0884cfa554ff5389a452d11065233a40863d0f
Dmitry Boulytchev <dboulytchev@gmail.com>
2018-02-25 21:11:06 +0300

    Fixed common.mlyet again

--- regression/common.ml
+++ regression/common.ml
@@ -9,7 +9,7 @@ let state ps = List.fold_right (fun (x, v) (s, p) -> Expr.update x v s, (x =:= !
 let eval  (s, p) e =
   let orig      = Expr.eval s e in
   let stmt      = List.fold_right (fun p s -> p |> s) p (Stmt.Write e) in
-  let [s_orig]  = eval [] stmt in
+  let [s_orig]  = Stmt.eval [] stmt in
   let [sm_orig] = SM.run [] (SM.compile stmt) in
   if conj (orig = s_orig) (orig = sm_orig)
   then Printf.printf "%d\n" orig
