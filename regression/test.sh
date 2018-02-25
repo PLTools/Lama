547d91a0440b4f0091cd6a5d9249a40187758924
Dmitry Boulytchev <dboulytchev@gmail.com>
2018-02-25 18:26:15 +0300

    Parser

--- regression/test.sh
+++ regression/test.sh
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-make clean
-make TOPFILE=test000
-=======
 make check
 pushd expressions && make check && popd
 pushd deep-expressions && make check && popd
-
->>>>>>> 30537ff6ba7f9d05ac3ef20249c10cfaa701f5a7
