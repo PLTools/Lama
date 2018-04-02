5c7cb4ccaa8f4c5cb2d15580bb48303aa8e36869
Dmitry Boulytchev <dboulytchev@gmail.com>
2018-04-02 05:58:02 +0300

    Revert "hw6"
    This reverts commit 2645f1433c10d80b14d12f9f80112de5960691d6.

--- src/Driver.ml
+++ src/Driver.ml
@@ -6,7 +6,7 @@ let parse infile =
     (object
        inherit Matcher.t s
        inherit Util.Lexers.decimal s
-       inherit Util.Lexers.ident ["read"; "write"; "skip"; "if"; "then"; "else"; "elif"; "fi"; "while"; "do"; "od"; "repeat"; "until"; "for"; (* add new keywords *)] s
+       inherit Util.Lexers.ident ["read"; "write"; "skip"; "if"; "then"; "else"; "elif"; "fi"; "while"; "do"; "od"; "repeat"; "until"; "for"; "fun"; "local"] s
        inherit Util.Lexers.skip [
 	 Matcher.Skip.whitespaces " \t\n";
 	 Matcher.Skip.lineComment "--";
