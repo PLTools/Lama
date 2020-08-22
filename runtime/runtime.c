f08cd8396f8f8b6d6206efd1d102634c043c182b
Dmitry Boulytchev <dboulytchev@gmail.com>
2020-08-22 20:11:41 +0300

    Data/Random/Buffer, spec updated

--- runtime/runtime.c
+++ runtime/runtime.c
@@ -1512,7 +1512,7 @@ extern void __gc_root_scan_stack ();
 /* ======================================== */
 
 //static size_t SPACE_SIZE = 16;
-static size_t SPACE_SIZE = 64 * 1024 * 1024;
+static size_t SPACE_SIZE = 256 * 1024 * 1024;
 // static size_t SPACE_SIZE = 128;
 // static size_t SPACE_SIZE = 1024 * 1024;
 
--- src/version.ml
+++ src/version.ml
@@ -1,1 +1,1 @@
-let version = "Version 1.00, 6f9d0850c, Fri Aug 7 21:40:51 2020 +0300"
+let version = "Version 1.00, 94e4b1626, Mon Aug 10 20:55:10 2020 +0300"
