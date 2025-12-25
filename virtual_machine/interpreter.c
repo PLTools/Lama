5e8c53932e3ba979c749ef784bdae4f3909db207
ancavar <danaj2004@yandex.ru>
2025-12-25 10:35:07 +0300

    add calls, stack frames

--- virtual_machine/interpreter.c
+++ virtual_machine/interpreter.c
@@ -11,17 +11,23 @@ typedef enum {
   OP_BINOP_MOD = 0x05,
   OP_CONST = 0x10,
   OP_END = 0x16,
+  OP_RET = 0x17,
   OP_DROP = 0x18,
   OP_DUP = 0x19,
   OP_SWAP = 0x1A,
   OP_LD = 0x20,
+  OP_LD_LOC = 0x21,
+  OP_LD_ARG = 0x22,
   OP_ST = 0x40,
+  OP_ST_LOC = 0x41,
+  OP_ST_ARG = 0x42,
   OP_BEGIN = 0x52,
   OP_BEGIN_CLOSURE = 0x53,
+  OP_CALL = 0x56,
   OP_LINE = 0x5A,
   OP_READ = 0x70,
   OP_WRITE = 0x71,
-  OP_STOP = 0xFF,
+  OP_HALT = 0xFF,
 } opcode_t;
 
 typedef struct {
@@ -106,46 +112,71 @@ bytecode *load_bytecode(const char *filename) {
 }
 
 #define STACK_SIZE 1024
-static int stack[STACK_SIZE];
-static int sp = 0;
 
-static void push(int val) {
-  if (sp >= STACK_SIZE) {
-    fprintf(stderr, "Stack overflow\n");
-    exit(1);
-  }
-  stack[sp++] = val;
+static inline void push(int **sp, int val) {
+  *(*sp)++ = val;
 }
 
-static int pop(void) {
-  if (sp <= 0) {
-    fprintf(stderr, "Cannot pop from an empty stack");
-    exit(1);
-  }
-  return stack[--sp];
+static inline int pop(int **sp) {
+  return *--(*sp);
 }
 
-static int peek(void) {
-  if (sp <= 0) {
-    fprintf(stderr, "Cannot peek from an empty stack");
-    exit(1);
-  }
-  return stack[sp - 1];
+static inline int peek(int **sp) {
+  return *(*sp - 1);
+}
+
+typedef struct frame {
+  struct frame *parent;
+  int return_ip;
+  int n_args;
+  int n_locals;
+  int locals[];
+} frame;
+
+static frame *current_frame = NULL;
+static int return_ip = -1;
+
+static frame *frame_create(frame *parent, int ret_ip, int n_args, int n_locals) {
+  int total_locals = n_args + n_locals;
+  frame *f = malloc(sizeof(frame) + total_locals * sizeof(int));
+  f->parent = parent;
+  f->return_ip = ret_ip;
+  f->n_args = n_args;
+  f->n_locals = n_locals;
+  memset(f->locals, 0, total_locals * sizeof(int));
+  return f;
+}
+
+static int *frame_local(frame *f, int idx) {
+  return &f->locals[idx];
+}
+
+static int *frame_arg(frame *f, int idx) {
+  return &f->locals[f->n_locals + idx];
+}
+
+static void frame_drop(frame *f) {
+  free(f);
 }
 
 void run(bytecode *bc) {
+  int stack[STACK_SIZE];
+  int *sp = stack;
   int *globals = malloc(sizeof(int) * bc->globals_count);
   int ip = bc->entry_point;
+  int pending_args = 0;
 
   while (ip < bc->code_size) {
     uint8_t opcode = bc->code[ip++];
     int l = opcode & 0xF;
 
+    printf("ip=%d opcode=0x%02X\n", ip, opcode);
+
     switch (opcode) {
     case OP_CONST: {
       int n = read_i32(bc->code, ip);
       ip += 4;
-      push(n);
+      push(&sp, n);
       break;
     }
     case OP_BINOP_ADD:
@@ -153,8 +184,8 @@ void run(bytecode *bc) {
     case OP_BINOP_MUL:
     case OP_BINOP_DIV:
     case OP_BINOP_MOD: {
-      int y = pop();
-      int x = pop();
+      int y = pop(&sp);
+      int x = pop(&sp);
       int result;
       switch (l) {
       case 1:
@@ -181,43 +212,104 @@ void run(bytecode *bc) {
         result = x % y;
         break;
       }
-      push(result);
+      push(&sp, result);
       break;
     }
     case OP_LD: {
       int idx = read_i32(bc->code, ip);
       ip += 4;
-      push(globals[idx]);
+      push(&sp, globals[idx]);
+      break;
+    }
+    case OP_LD_LOC: {
+      int idx = read_i32(bc->code, ip);
+      ip += 4;
+      push(&sp, *frame_local(current_frame, idx));
+      break;
+    }
+    case OP_LD_ARG: {
+      int idx = read_i32(bc->code, ip);
+      ip += 4;
+      push(&sp, *frame_arg(current_frame, idx));
       break;
     }
     case OP_ST: {
       int idx = read_i32(bc->code, ip);
       ip += 4;
-      int val = pop();
+      int val = pop(&sp);
       globals[idx] = val;
-      push(val);
+      push(&sp, val);
+      break;
+    }
+    case OP_ST_LOC: {
+      int idx = read_i32(bc->code, ip);
+      ip += 4;
+      int val = pop(&sp);
+      *frame_local(current_frame, idx) = val;
+      push(&sp, val);
+      break;
+    }
+    case OP_ST_ARG: {
+      int idx = read_i32(bc->code, ip);
+      ip += 4;
+      int val = pop(&sp);
+      *frame_arg(current_frame, idx) = val;
+      push(&sp, val);
       break;
     }
     case OP_DROP:
-      pop();
+      pop(&sp);
       break;
-    case OP_DUP: {
-      int x = peek();
-      push(x);
+    case OP_DUP:
+      push(&sp, peek(&sp));
       break;
-    }
     case OP_SWAP: {
-      int y = pop();
-      int x = pop();
-      push(y);
-      push(x);
+      int y = pop(&sp);
+      int x = pop(&sp);
+      push(&sp, y);
+      push(&sp, x);
       break;
     }
-    case OP_BEGIN:
+    case OP_BEGIN: {
+      int n_args = read_i32(bc->code, ip);
+      ip += 4;
+      int n_locals = read_i32(bc->code, ip);
+      ip += 4;
+      frame *new_frame = frame_create(current_frame, return_ip, n_args, n_locals);
+      for (int i = n_args - 1; i >= 0; i--) {
+        *frame_arg(new_frame, i) = pop(&sp);
+      }
+      current_frame = new_frame;
+      break;
+    }
     case OP_BEGIN_CLOSURE:
       // TODO: skip for now
       ip += 8;
       break;
+    case OP_CALL: {
+      int addr = read_i32(bc->code, ip);
+      ip += 4;
+      // discarding n_args
+      ip += 4;
+      return_ip = ip;
+      ip = addr;
+      break;
+    }
+    case OP_RET:
+    case OP_END: {
+      if (current_frame == NULL) {
+        goto end;
+      }
+      int ret_ip = current_frame->return_ip;
+      frame *parent = current_frame->parent;
+      frame_drop(current_frame);
+      current_frame = parent;
+      if (ret_ip < 0) {
+        goto end;
+      }
+      ip = ret_ip;
+      break;
+    }
     case OP_READ: {
       int x;
       // TODO: scanf ?
@@ -225,17 +317,16 @@ void run(bytecode *bc) {
         fprintf(stderr, "Failed to read\n");
         goto end;
       }
-      push(x);
+      push(&sp, x);
       break;
     }
     case OP_WRITE: {
-      int x = pop();
+      int x = pop(&sp);
       printf("%d\n", x);
-      push(x);
+      push(&sp, x);
       break;
     }
-    case OP_END:
-    case OP_STOP:
+    case OP_HALT:
       goto end;
     case OP_LINE:
       ip += 4;
@@ -247,6 +338,11 @@ void run(bytecode *bc) {
   }
 
 end:
+  while (current_frame) {
+    frame *parent = current_frame->parent;
+    frame_drop(current_frame);
+    current_frame = parent;
+  }
   free(globals);
 }
 
