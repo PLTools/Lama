6300f5f37cdd3858cebea9e0d0efe926e682b52e
ancavar <danaj2004@yandex.ru>
2026-03-01 01:19:20 +0300

    bring back `opcodes.c` for easier debugging

---
+++ virtual_machine/opcodes.c
@@ -0,0 +1,111 @@
+#include "opcodes.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+const char *opcode_to_string(uint8_t opcode) {
+  switch ((opcode_t)opcode) {
+  case OP_BINOP_ADD:
+    return "ADD";
+  case OP_BINOP_SUB:
+    return "SUB";
+  case OP_BINOP_MUL:
+    return "MUL";
+  case OP_BINOP_DIV:
+    return "DIV";
+  case OP_BINOP_MOD:
+    return "MOD";
+  case OP_BINOP_LT:
+    return "LT";
+  case OP_BINOP_LE:
+    return "LE";
+  case OP_BINOP_GT:
+    return "GT";
+  case OP_BINOP_GE:
+    return "GE";
+  case OP_BINOP_EQ:
+    return "EQ";
+  case OP_BINOP_NE:
+    return "NE";
+  case OP_BINOP_AND:
+    return "AND";
+  case OP_BINOP_OR:
+    return "OR";
+  case OP_CONST:
+    return "CONST";
+  case OP_STRING:
+    return "STRING";
+  case OP_SEXP:
+    return "SEXP";
+  case OP_STA:
+    return "STA";
+  case OP_JMP:
+    return "JMP";
+  case OP_END:
+    return "END";
+  case OP_DROP:
+    return "DROP";
+  case OP_DUP:
+    return "DUP";
+  case OP_SWAP:
+    return "SWAP";
+  case OP_ELEM:
+    return "ELEM";
+  case OP_LD:
+    return "LD";
+  case OP_LD_LOC:
+    return "LD.LOC";
+  case OP_LD_ARG:
+    return "LD.ARG";
+  case OP_LD_CLO:
+    return "LD.CLO";
+  case OP_ST:
+    return "ST";
+  case OP_ST_LOC:
+    return "ST.LOC";
+  case OP_ST_ARG:
+    return "ST.ARG";
+  case OP_ST_CLO:
+    return "ST.CLO";
+  case OP_CJMP_Z:
+    return "CJMP.Z";
+  case OP_CJMP_NZ:
+    return "CJMP.NZ";
+  case OP_BEGIN:
+    return "BEGIN";
+  case OP_BEGIN_CLOSURE:
+    return "BEGIN.CLO";
+  case OP_CLOSURE:
+    return "CLOSURE";
+  case OP_CALLC:
+    return "CALLC";
+  case OP_CALL:
+    return "CALL";
+  case OP_TAG:
+    return "TAG";
+  case OP_ARRAY:
+    return "ARRAY";
+  case OP_FAIL:
+    return "FAIL";
+  case OP_LINE:
+    return "LINE";
+  case OP_PATT_STR_CMP:
+    return "PATT.STRCMP";
+  case OP_PATT_STRING:
+    return "PATT.STRING";
+  case OP_PATT_ARRAY:
+    return "PATT.ARRAY";
+  case OP_PATT_SEXP:
+    return "PATT.SEXP";
+  case OP_PATT_BOXED:
+    return "PATT.BOXED";
+  case OP_PATT_UNBOXED:
+    return "PATT.UNBOXED";
+  case OP_PATT_CLOSURE:
+    return "PATT.CLOSURE";
+  case OP_BARRAY:
+    return "BARRAY";
+  default:
+    fprintf(stderr, "Unknown opcode: %d\n", opcode);
+    exit(EXIT_FAILURE);
+  }
+}
